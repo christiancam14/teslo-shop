5f5d6c36428118ca5f77a9088015ec00d9403a14
Christian Stiven Camacho Galvis <chriscamacho1045@gmail.com>
2024-05-03 19:50:41 -0500

    Preparacion update

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,7 +3,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { validate as IsUUID } from 'uuid';
 import { isUUID } from 'class-validator';
@@ -19,7 +19,9 @@ export class ProductsService {
     private readonly productRepository: Repository<Product>,
 
     @InjectRepository(ProductImage)
-    private readonly productImageRepository: Repository<ProductImage>
+    private readonly productImageRepository: Repository<ProductImage>,
+
+    private readonly dataSoruce: DataSource
   ) { }
 
   async create(createProductDto: CreateProductDto) {
@@ -132,15 +134,16 @@ export class ProductsService {
 
   async update(id: string, updateProductDto: UpdateProductDto) {
 
-    const product = await this.productRepository.preload({
-      id: id,
-      ...updateProductDto,
-      images: []
-    })
+    const { images, ...toUpdate} = updateProductDto;
+
+    const product = await this.productRepository.preload({ id: id, ...toUpdate })
 
     if (!product) {
       throw new NotFoundException(`Product with id: ${id} not found`);
     }
+
+    // Crear query runner
+    const queryRunner = this.dataSoruce.createQueryBuilder();
 
     try {
 
